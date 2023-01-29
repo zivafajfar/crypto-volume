3e3c0dd3ed5711d73ad253c709596ee3ef098478
Ziva F <ziva@MBP-de-Ziva.lan>
2023-01-29 21:37:24 +0100

    modified app.js

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 let response = null;
 new Promise(async (resolve, reject) => {
   try {
-    response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?volume_24h_min=10000000&market_cap_max=10000000000000&limit=5000', {
+    response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?volume_24h_min=1000000&market_cap_max=10000000000000&limit=5000', {
       headers: {
         'X-CMC_PRO_API_KEY': 'd996075f-c301-4197-8ed6-f964982d64fc',
       },
@@ -35,10 +35,10 @@ new Promise(async (resolve, reject) => {
 
     let volume = ""
     for (let i = 1; i < resultFiltered.length; i++) {
-      volume += "<tr><td>" + i + "</td>" + "<td>" + resultFiltered[i].symbol + "</td>" + "<td>" + resultFiltered[i].name + "</td>" + "<td>" + resultFiltered[i].quote.USD.volume_24h + "</td>" + "<td>" + resultFiltered[i].quote.USD.volume_change_24h + "</td></tr>"
+      volume += "<tr><td>" + i + "</td>" + "<td>" + resultFiltered[i].symbol + "</td>" + "<td>" + resultFiltered[i].name + "</td>" + "<td>" + resultFiltered[i].quote.USD.volume_24h.toLocaleString(undefined,{ minimumFractionDigits: 2 } + "</td>" + "<td>" + resultFiltered[i].quote.USD.volume_change_24h + "</td></tr>"
     }
     app.get('/', (req, res) => {
-      res.send("<table><tr><th>" + "</th>" + "<th>SYMBOL</th>" + "<th>NAME</th>" + "<th>VOLUME</th>" + "<th>24H VOLUME CHANGE</th></tr>" + volume + "</table>");
+      res.send("<table><tr><th>" + "</th>" + "<th>SYMBOL</th>" + "<th>NAME</th>" + "<th>VOLUME</th>" + "<th>24H VOLUME CHANGE (%)</th></tr>" + volume + "</table>");
     });
 
   }
